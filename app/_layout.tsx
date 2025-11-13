22085bb5cc84a56d6ec79cc59eadd046c2b67185
“buruei” <buruei@hotmail.com>
2025-11-12 21:12:10 -0500

    feat: Apply notifications and Sessions Context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,36 @@
 // app/_layout.tsx
-import React from "react";
-import { Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { Stack, useRouter } from "expo-router";
 import { ThemeProvider } from "../lib/ThemeContext";
+import { SessionsProvider } from "../lib/SessionsContext";
+import { registerNotificationResponseListener } from "../lib/notifications";
+
+function NotificationBridge() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const responseSub = registerNotificationResponseListener((response) => {
+      const targetScreen = response.notification.request.content.data?.screen;
+      if (typeof targetScreen === "string") {
+        router.push(targetScreen);
+      }
+    });
+
+    return () => {
+      responseSub.remove();
+    };
+  }, [router]);
+
+  return null;
+}
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <Stack screenOptions={{ headerShown: false }} />
-    </ThemeProvider>
+    <SessionsProvider>
+      <ThemeProvider>
+        <NotificationBridge />
+        <Stack screenOptions={{ headerShown: false, animation: "fade", animationDuration: 10 }} />
+      </ThemeProvider>
+    </SessionsProvider>
   );
 }
